8bad21987367cfe6c5f9a30a36f723cd0c7bb468
Henry Paradiz <henry.paradiz@gmail.com>
2023-07-26 09:22:17 -0700

    Backups! An unfinished media page
    Divergence framework 2.1.0

--- src/Controllers/Admin.php
+++ src/Controllers/Admin.php
@@ -9,13 +9,16 @@
  */
 namespace technexus\Controllers;
 
+use Divergence\Helpers\Util;
+use Ifsnop\Mysqldump;
 use \technexus\App as App;
-
+use Divergence\IO\Database\MySQL;
 use Divergence\Responders\Response;
 use Divergence\Responders\TwigBuilder;
 use Psr\Http\Message\RequestInterface;
-use Divergence\IO\Database\MySQL as DB;
+use Divergence\Responders\MediaBuilder;
 use Psr\Http\Message\ResponseInterface;
+use Divergence\Responders\MediaResponse;
 use \technexus\Models\BlogPost as BlogPost;
 
 /**
@@ -147,8 +150,48 @@ class Admin extends \Divergence\Controllers\RequestHandler
      */
     public function backups(): ResponseInterface
     {
-        return new Response(new TwigBuilder('admin/backups.twig', [
-            //'Users' => \technexus\Models\User::getAll(),
-        ]));
+        switch ($action = $this->shiftPath()) {
+            case '':
+                return new Response(new TwigBuilder('admin/backups.twig', [
+                    'configurations'
+                ]));;
+            
+            case 'download':
+                return $this->downloadBackup();
+        }
+
+    }
+
+    public function downloadBackup(): ResponseInterface
+    {
+        try {
+            $dbConfig = App::$App->config('db');
+            $config = $dbConfig[MySQL::$currentConnection];
+            
+            $tmpName = tempnam('/tmp','db');
+
+            $dump = new Mysqldump\Mysqldump('mysql:host='.$config['host'].';dbname='.$config['database'], $config['username'], $config['password'],[
+                'compress' => Mysqldump\Mysqldump::BZIP2 
+            ]);
+            $dump->start($tmpName);
+
+        } catch (\Exception $e) {
+            throw $e;
+        } 
+
+        $this->responseBuilder = MediaBuilder::class;
+        $className = $this->responseBuilder;
+        $responseBuilder = new $className($tmpName, []);
+
+        $responseBuilder->setContentType('application/x-bzip2');
+
+        $response = new MediaResponse($responseBuilder);
+        $filename = 'backup-'.$_SERVER['SERVER_NAME'].'-'.date('Y-m-d').'.sql.bz2';
+
+        $response = $response->withHeader('Content-Disposition', 'attachment; filename="'.$filename.'"');
+        $response = $response->withHeader('ETag', 'backup-'.$tmpName)
+            ->withHeader('Content-Length', filesize($tmpName));
+
+        return $response;
     }
 }
